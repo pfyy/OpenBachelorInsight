f523e2d251320467366cbba8f1ffd39980abd0f4
pfyy <112098515+pfyy@users.noreply.github.com>
2026-04-30 22:28:47 +0800

    actual write

--- src/openbachelori/main.py
+++ src/openbachelori/main.py
@@ -1,16 +1,33 @@
-from flask import Flask
+import json
+
+
+from flask import Flask, request
+import jsonlines
+
+
+import openbachelori.config
+
 
 app = Flask(__name__)
 
 
+obi_state_dict: dict = None
+
+
 @app.route("/obi/begin", methods=["POST"])
 def obi_begin():
-    pass
+    global obi_state_dict
+    obi_state_dict = {}
 
 
 @app.route("/obi/end", methods=["POST"])
 def obi_end():
-    pass
+    global obi_state_dict
+
+    with jsonlines.open("data/tmp.jsonl", mode="a") as writer:
+        writer.write(obi_state_dict)
+
+    obi_state_dict = None
 
 
 if __name__ == "__main__":
