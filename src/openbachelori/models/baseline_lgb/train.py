ac612b585816fb3b4b4029f677901a85eb6f34e6
pfyy <112098515+pfyy@users.noreply.github.com>
2026-05-04 09:50:32 +0800

    more output

--- src/openbachelori/models/baseline_lgb/train.py
+++ src/openbachelori/models/baseline_lgb/train.py
@@ -6,6 +6,8 @@ from sklearn.metrics import (
     recall_score,
     f1_score,
     roc_auc_score,
+    confusion_matrix,
+    classification_report,
 )
 import lightgbm as lgb
 
@@ -35,11 +37,17 @@ def main():
 
     auc = roc_auc_score(y_test, y_pred_proba)
 
+    cm = confusion_matrix(y_test, y_pred)
+
     print("accuracy_score:", acc)
     print("precision_score:", pre)
     print("recall_score:", rec)
     print("f1_score:", f1)
     print("roc_auc_score:", auc)
+    print(
+        f"confusion_matrix:\n{cm}",
+    )
+    print(f"classification_report:\n{classification_report(y_test, y_pred)}")
 
 
 if __name__ == "__main__":
