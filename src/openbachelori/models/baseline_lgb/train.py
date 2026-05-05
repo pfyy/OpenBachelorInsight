5e44c2fe9f8541773cbed0afe10aabb8e3960d9d
pfyy <112098515+pfyy@users.noreply.github.com>
2026-05-05 10:25:21 +0800

    metric

--- src/openbachelori/models/baseline_lgb/train.py
+++ src/openbachelori/models/baseline_lgb/train.py
@@ -12,6 +12,32 @@ from sklearn.metrics import (
 import lightgbm as lgb
 
 
+def print_metrics(clf: lgb.LGBMClassifier, X, y):
+    print("----------")
+
+    y_pred = clf.predict(X)
+    y_pred_proba = clf.predict_proba(X)[:, 1]
+
+    acc = accuracy_score(y, y_pred)
+    pre = precision_score(y, y_pred)
+    rec = recall_score(y, y_pred)
+    f1 = f1_score(y, y_pred)
+
+    auc = roc_auc_score(y, y_pred_proba)
+
+    cm = confusion_matrix(y, y_pred)
+
+    print("accuracy_score:", acc)
+    print("precision_score:", pre)
+    print("recall_score:", rec)
+    print("f1_score:", f1)
+    print("roc_auc_score:", auc)
+    print(
+        f"confusion_matrix:\n{cm}",
+    )
+    print(f"classification_report:\n{classification_report(y, y_pred)}")
+
+
 def main():
     df = pd.read_csv("csv/multiOperationMatch_act3enemyduel_01b.csv")
 
@@ -27,27 +53,8 @@ def main():
     clf = lgb.LGBMClassifier(random_state=42)
     clf.fit(X_train, y_train)
 
-    y_pred = clf.predict(X_test)
-    y_pred_proba = clf.predict_proba(X_test)[:, 1]
-
-    acc = accuracy_score(y_test, y_pred)
-    pre = precision_score(y_test, y_pred)
-    rec = recall_score(y_test, y_pred)
-    f1 = f1_score(y_test, y_pred)
-
-    auc = roc_auc_score(y_test, y_pred_proba)
-
-    cm = confusion_matrix(y_test, y_pred)
-
-    print("accuracy_score:", acc)
-    print("precision_score:", pre)
-    print("recall_score:", rec)
-    print("f1_score:", f1)
-    print("roc_auc_score:", auc)
-    print(
-        f"confusion_matrix:\n{cm}",
-    )
-    print(f"classification_report:\n{classification_report(y_test, y_pred)}")
+    print_metrics(clf, X_train, y_train)
+    print_metrics(clf, X_test, y_test)
 
     clf.booster_.save_model("src/openbachelori/models/baseline_lgb/model.txt")
 
