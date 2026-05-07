3326646de115eb307b91bba8fbc38e952bd5b4ba
pfyy <112098515+pfyy@users.noreply.github.com>
2026-05-07 22:51:57 +0800

    multi tag

--- src/openbachelori/reader.py
+++ src/openbachelori/reader.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import sys
+from functools import lru_cache
 
 import frida
 import requests
@@ -7,8 +8,36 @@ import requests
 from openbachelori.models.baseline_lgb.pred import get_y_pred
 
 
+@lru_cache
+def get_multi_tag_dict():
+    return {
+        "enemy_15072_dqlbgg": [
+            "enemy_15070_dqhlgy",
+            "enemy_15071_dqyrzf",
+        ],
+        "enemy_15073_dqkght": [
+            "enemy_15074_dqdght",
+        ],
+        "enemy_15075_dqzklz": [
+            "enemy_15076_dqzmst",
+        ],
+    }
+
+
+def process_multi_tag(payload):
+    multi_tag_dict = get_multi_tag_dict()
+
+    for multi_tag, enemy_id_lst in multi_tag_dict.items():
+        if multi_tag in payload:
+            print(f"multi_tag {multi_tag} found in {payload}")
+            for enemy_id in enemy_id_lst:
+                payload[enemy_id] = payload[multi_tag]
+
+
 def script_on_message(message, data):
     payload = message["payload"]
+
+    process_multi_tag(payload)
 
     print("----------")
 
